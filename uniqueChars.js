cfb1947539c71663c56b87451c7c8fe4e90170d2
Franco Abaroa <franco9395@gmail.com>
2017-01-16 16:03:05 -0800

    Complete uniqueChars

--- uniqueChars.js
+++ uniqueChars.js
@@ -2,16 +2,31 @@
 can not use additional data structures? */
 
 function uniqueChars(str) {
-  str = str.split('');
-  var storage = {};
-  for (var i = 0; i < str.length; i++) {
-    if (storage[str[i]] === undefined) {
-      storage[str[i]] = str[i];
-    } else {
-      return false;
+  // str = str.split('');
+  // var storage = {};
+  // for (var i = 0; i < str.length; i++) {
+  //   if (storage[str[i]] === undefined) {
+  //     storage[str[i]] = str[i];
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  // return true;
+
+  var helperFunction = function(string, comparChar) {
+    var counter = 0;
+    for (var i = 0; i < str.length; i++) {
+      if (string[i] === comparChar) {
+        counter++;
+      }
+    }
+    return counter === 2 ? true : false;
+  }
+
+  for (var j = 0; j < str.length; j++) {
+    if (helperFunction(str, str[j])) {
+      return true;
     }
   }
-  return true;
+  return false;
 }
-
-console.log(uniqueChars('helordo'));
