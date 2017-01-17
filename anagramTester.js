4e960cd3453785e3d6a339f3676a5414ddb1696b
Franco Abaroa <franco9395@gmail.com>
2017-01-16 16:05:09 -0800

    Remove repeat line of code

--- anagramTester.js
+++ anagramTester.js
@@ -1,7 +1,5 @@
 /* Write a method to decide if two strings are anagrams or not. */
 
 function anagramTester(str1, str2) {
-  str1 = str1.split('').sort().join('');
-  str2 = str2.split('').sort().join('');
-  return str1 === str2;
+  return str1.split('').sort().join('') === str2.split('').sort().join('');
 }
